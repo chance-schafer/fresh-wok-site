15ab4685d09118e20776ad349b5f87d42f3ee5f2
Chance Schafer <chance.schafer.dev@gmail.com>
2022-05-22 11:42:24 -0500

    finished food section

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ menuBtn.onclick = () => {
 };
 
 let swiper = new Swiper('.home-slider', {
+  speed: 750,
   grabCursor: true,
   loop: true,
   centeredSlides: true,
@@ -15,3 +16,49 @@ let swiper = new Swiper('.home-slider', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+let swiperTwo = new Swiper('.food-slider', {
+  speed: 750,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+let previewContainer = document.querySelector('.food-preview-container');
+let previewBox = previewContainer.querySelectorAll('.food-preview');
+
+document.querySelectorAll('.food .slide').forEach(food =>{
+  food.onclick = () =>{
+    previewContainer.style.display = 'flex';
+    let name = food.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewContainer.querySelector('#close-preview').onclick = () =>{
+  previewContainer.style.display = 'none';
+  previewBox.forEach(close =>{
+    close.classList.remove('active');
+  });
+}
